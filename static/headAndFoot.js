a91ced8c177ec3512318e537e97af5b6430600ab
Kestron <kestron@kestron.software>
2024-01-12 18:11:55 -0800

    Add files via upload

---
+++ static/headAndFoot.js
@@ -0,0 +1,9 @@
+document.body.innerHTML=`
+<div id="banner">
+    <a href="/"><img src="stewbot.jpg" width="7%"><img src="banner.png" width="40%"></a><br>
+    <span class="green"><b class="cyan">S</b>omething <b class="cyan">T</b>o <b class="cyan">E</b>xpedite <b class="cyan">W</b>ork</span><br><br>
+    <a class="bannerBtn" href="features">Features</a><a class="bannerBtn" href="commands">Commands</a><a href="pricing" class="bannerBtn">Pricing</a><a href="add" class="bannerBtn">Add to Server</a><a class="bannerBtn" href="/source">Source Code</a>
+</div>
+<br>
+${document.body.innerHTML}
+<footer>Developed by <a href="https://github.com/Kestron06">Kestron</a>. Special thanks to <a href="https://github.com/Reginald-Gillespie">WKoA</a> for providing hosting. Join our Discord support server here: <a href="https://discord.gg/k3yVkrrvez">Discord Support Server</a></footer>`;
