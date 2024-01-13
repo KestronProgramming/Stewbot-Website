562e05d8d7d837fb8cc966da5115fd6386a1460e
Kestron06 <kestron@kestron.software>
2024-01-12 23:24:30 -0800

    Mobile Layout

--- static/headAndFoot.js
+++ static/headAndFoot.js
@@ -1,6 +1,6 @@
 document.body.innerHTML=`
 <div id="banner">
-    <a href="/"><img src="stewbot.jpg" width="7%"><img src="banner.png" width="40%"></a><br>
+    <a href="/"><img src="stewbot.jpg" id="stewIcon"><img src="banner.png" id="stewBanner"></a><br>
     <span class="green"><b class="cyan">S</b>omething <b class="cyan">T</b>o <b class="cyan">E</b>xpedite <b class="cyan">W</b>ork</span><br><br>
     <a class="bannerBtn" href="features">Features</a><a class="bannerBtn" href="commands">Commands</a><a href="pricing" class="bannerBtn">Pricing</a><a href="add" class="bannerBtn">Add to Server</a><a class="bannerBtn" href="/source">Source Code</a>
 </div>
