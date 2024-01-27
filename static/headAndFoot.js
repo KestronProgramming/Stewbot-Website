db8962cf48566c0235e73f6b8fc1614eda86a9c4
Kestron <kestron@kestron.software>
2024-01-27 01:09:48 -0800

    Update headAndFoot.js

--- static/headAndFoot.js
+++ static/headAndFoot.js
@@ -1,7 +1,7 @@
 document.body.innerHTML=`
 <div id="banner">
     <a href="/"><img src="stewbot.jpg" id="stewIcon"><img src="banner.png" id="stewBanner"></a><br>
-    <span class="green"><b class="cyan">S</b>omething <b class="cyan">T</b>o <b class="cyan">E</b>xpedite <b class="cyan">W</b>ork</span><br><br>
+    <span class="green"><b class="cyan">S</b>teward <b class="cyan">T</b>o <b class="cyan">E</b>xpedite <b class="cyan">W</b>ork</span><br><br>
     <a class="bannerBtn" href="features">Features</a><a class="bannerBtn" href="commands">Commands</a><a href="pricing" class="bannerBtn">Pricing</a><a href="add" class="bannerBtn">Add to Server</a><a class="bannerBtn" href="/source">Source Code</a>
 </div>
 <br>
